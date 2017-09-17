b838749ec9c07012cf1e4ff473fb6f43d0ad7934
koskh <koskh@yandex.ru>
2017-09-17 11:56:41 +0200

    #master /listen_2

--- server/src/services/bd/Lesson_2.js
+++ server/src/services/bd/Lesson_2.js
@@ -3,7 +3,37 @@
 module.exports = {
     id: 2,
     words: [
-        { foreign: ['hare'], native: ['заяц'], sounds: ['/sounds/h/hare.ogg'] }
+        { foreign: ['hinge'], native: ['петля (дверная)', 'шарнир'], sounds: ['/sounds/h/hinge.ogg'] },
+        { foreign: ['spell'], native: ['заклинание', ' писать или произносить (слово) по буквам', 'период (чего-л.)'], sounds: ['/sounds/s/spell.ogg'] },
+        { foreign: ['sleep (slept,slept )'], native: ['сон; дремота', 'спать (прям и пренеосн смысл)'], sounds: ['/sounds/s/sleep.ogg'] },
+        { foreign: ['steel'], native: ['сталь', 'твёрдость', 'холодное оружие; меч, шпага'], sounds: ['/sounds/s/steel.ogg'] },
+        { foreign: ['even'], native: ['плоский', 'однородный, равномерный (о действ., процесc., сост.)', 'выравнивать, сглаживать', 'даже'], sounds: ['/sounds/i/even.ogg'] },
+        { foreign: ['though'], native: ['хотя, несмотря на', 'тем не менее', 'всё-таки'], sounds: ['/sounds/t/though.ogg'] },
+        { foreign: ['peep'], native: ['беглый взгляд', 'тайный взгляд', 'заглядывать (куда-л.)'], sounds: ['/sounds/p/peep.ogg'] },
+        { foreign: ['file'], native: ['надфиль', 'шлифовка', 'скоросшиватель', 'файл', 'дело; досье', 'регистрировать (документы)', 'подшивать к делу'], sounds: ['/sounds/f/file.ogg'] },
+        { foreign: ['time'], native: ['время', 'срок, продолжительность', 'раз '], sounds: ['/sounds/t/time.ogg'] },
+        { foreign: ['slide (slid, slid)'], native: ['скользить (прям. и переносн.)'], sounds: ['/sounds/s/slide.ogg'] },
+        { foreign: ['find (found, found)'], native: ['находить, встречать', 'находка'], sounds: ['/sounds/f/find.ogg'] },
+        { foreign: ['fine'], native: ['штраф', 'штрафовать', 'тонкий, утончённый (прям. и перен.)', 'прекрасный, высококачественный', 'очищать'], sounds: ['/sounds/f/fine.ogg'] },
+        { foreign: ['lock'], native: ['замок', 'шлюз', 'запирать', 'стискивать'], sounds: ['/sounds/l/lock.ogg'] },
+        { foreign: ['line'], native: ['верёвка', 'провод', 'линия', 'располагать в линию'], sounds: ['/sounds/l/line.ogg'] },
+        { foreign: ['steal (stole, stolen)'], native: ['воровать'], sounds: ['/sounds/s/steal.ogg'] },
+        { foreign: ['eye'], native: ['глаз', 'взгляд'], sounds: ['/sounds/e/eye.ogg'] },
+        { foreign: ['buy (bought, bought)'], native: ['покупать', '"купиться" на что-л.'], sounds: ['/sounds/b/buy.ogg'] },
+        { foreign: ['sell (sold , sold )'], native: ['продавать', 'раскупаться', '"втюхивать" что-л.'], sounds: ['/sounds/s/sell.ogg'] },
+        { foreign: ['sell (sold , sold )'], native: ['продавать', 'раскупаться', '"втюхивать" что-л.'], sounds: ['/sounds/s/sell.ogg'] },
+        { foreign: ['knob'], native: ['выпуклость', '(тех.) ручка; головка; кнопка', 'выпячиваться, раздуваться'], sounds: ['/sounds/k/knob.ogg'] },
+        { foreign: ['grip'], native: ['сжать', 'схватить (прям. и перен.)', 'захват'], sounds: ['/sounds/g/grip.ogg'] },
+        { foreign: ['till'], native: ['до', 'не раньше', 'пока'], sounds: ['/sounds/t/till.ogg'] },
+        { foreign: ['bill'], native: ['счёт', 'выставлять счёт'], sounds: ['/sounds/b/bill.ogg'] },
+        { foreign: ['list'], native: ['список', 'вносить в список'], sounds: ['/sounds/l/list.ogg'] },
+        { foreign: ['little (less, least)'], native: ['маленький', 'незначительный'], sounds: ['/sounds/l/little.ogg'] },
+        { foreign: ['signboard'], native: ['вывеска'], sounds: ['/sounds/s/signboard.ogg'] },
+        { foreign: ['settle'], native: ['урегулировать, разрешить (спор, конфликт)', 'поселиться', 'оседать (об осдаке, пыле, снеге)'], sounds: ['/sounds/s/settle.ogg'] },
+        { foreign: ['listen'], native: ['слушать', 'прислушиваться'], sounds: ['/sounds/l/listen.ogg'] },
+        { foreign: ['so'], native: ['(именно) так, таким образом'], sounds: ['/sounds/s/so.ogg'] },
+        { foreign: ['wet'], native: ['мокрый', 'дождливая погода'], sounds: ['/sounds/w/wet.ogg'] },
+        { foreign: ['admit'], native: ['допускать (что-л.)', 'впускать'], sounds: ['/sounds/a/admit.ogg'] }
     ],
     title: 'Lesson 2. Door.'
 };
