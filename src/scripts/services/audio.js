516ff7851ffb31a9ffb566115396d4eb07070ea8
koskh <koskh@yandex.ru>
2017-09-21 19:04:40 +0200

    #master application sound service

--- src/scripts/services/audio.js
+++ src/scripts/services/audio.js
@@ -1,9 +1,23 @@
-
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = AudioContext && new AudioContext();
 
 let audioSrv = null;
+const encoder = new TextEncoder();
 
+// const proxyStorage = {
+//
+//     set: (url, int16array) => {
+//         localStorage.setItem(url, int16array);
+//     },
+//
+//     get: url => {
+//         const bufferString = localStorage.getItem(url);
+//
+//         return bufferString ? new Int16Array(encoder.encode(bufferString).buffer) : undefined;
+//     }
+// };
+
+const proxyStorage = {};
 
 function play(url) {
     if (!audioCtx) {
@@ -12,24 +26,40 @@ function play(url) {
         audio.load();
         audio.play();
     } else {
+        // if (proxyStorage.get(url)) {
+        //     _decodeAudio(proxyStorage.get(url));
+        //     return;
+        // }
+
         const request = new XMLHttpRequest();
         request.open('GET', url, true);
         request.responseType = 'arraybuffer';
         request.send();
 
         request.onload = () => {
-            audioCtx.decodeAudioData(request.response, buffer => {
-                const source = audioCtx.createBufferSource();
-                source.buffer = buffer;
-                source.connect(audioCtx.destination);
-                source.start(0);
-            });
+            // proxyStorage[url] = new Int16Array(request.response, 0, Math.floor(request.response.byteLength / 2)).toString();
+            proxyStorage[url] = new Int16Array(request.response, 0, Math.floor(request.response.byteLength / 2));
+            _decodeAudio(proxyStorage[url]);
         };
     }
 }
 
+function _decodeAudio(array) {
+    // const fullBuffer = (new Int16Array(encoder.encode(arrayString).buffer)).buffer;
+    const fullBuffer = new Int16Array(array).buffer;
+
+    debugger
+
+    audioCtx.decodeAudioData(fullBuffer, buffer => {
+        const source = audioCtx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(audioCtx.destination);
+        source.start(0);
+    });
+}
+
 function initialize() {
-    audioSrv = audioSrv || {play};
+    audioSrv = audioSrv || { play };
     return audioSrv;
 }
 
