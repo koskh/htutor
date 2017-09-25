f37c6541cefe9f43268fc0cd5247faf82170a4e2
koskh <koskh@yandex.ru>
2017-09-25 14:54:44 +0200

    #master /test  реф верстик молулей теста

--- src/scripts/components/Navigation/index.js
+++ src/scripts/components/Navigation/index.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 // import style from './index.pcss';
 
 const Navigation = ({ match }) => (
-    <nav className={'nav bg-light mb'}>
+    <nav className={'nav bg-light mb-4'}>
         <NavLink to={`${match.url}home`} className="nav-link">Home</NavLink>
         <NavLink to={`${match.url}learn`} className="nav-link">Learn</NavLink>
         <NavLink to={`${match.url}test`} className="nav-link">Test</NavLink>
