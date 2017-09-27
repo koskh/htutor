363fe2c03611e4cbc15de0f6297343ff85c3ca39
koskh <koskh@yandex.ru>
2017-09-27 10:05:36 +0200

    #master /SpellTest, render w/error

--- src/scripts/components/Test/SpellTest/index.js
+++ src/scripts/components/Test/SpellTest/index.js
@@ -10,6 +10,31 @@ import { TemplateClass, foreignWordClasses } from '../TemplateClass';
 export default class SpellTest extends TemplateClass {
 
 
+    render(): React.Element<any> {
+        const { quizWord } = this.props;
+        const { foreignWordClass } = this.state;
 
+        return (
+            <div>
+                <div className="d-flex align-items-center justify-content-center quiz-head-height mb-4">
+                    <div className={cn('btn btn-lg btn-block', foreignWordClass, 'qa-quiz-place')}>
+                        <div className="row">
+                            <div className="col-2">
+                            &nbsp;
+                            </div>
+                            <div className="col-8 text-truncate qa-quiz-word">
+                                {quizWord}
+                            </div>
+                            <div className="col-2 ">
+                                &nbsp;
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
--- src/scripts/components/Test/SpellTest/index.spec.js
+++ src/scripts/components/Test/SpellTest/index.spec.js
@@ -22,5 +22,7 @@ describe('<SpellTest />', () => {
     });
 
 
+
+
 });
 
