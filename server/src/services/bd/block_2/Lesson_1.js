bf9156448783aedd6c06689ce51ed1ca2c045067
koskh <koskh@yandex.ru>
2017-10-16 20:25:36 +0200

    #master Block_2/Lesson_1, Block_2/Lesson_2

--- server/src/services/bd/block_2/Lesson_1.js
+++ server/src/services/bd/block_2/Lesson_1.js
@@ -3,7 +3,40 @@
 module.exports = {
     id: 1,
     words: [
-        { foreign: ['clever'], native: ['умный'], sounds: ['/sounds/c/clever.ogg'] }
+        { foreign: ['ramp'], native: ['наклонная плоскость, уклон', 'пандус', 'лежачий полицейский', 'становиться на дыбы, на задние лапы (в геральдике)'], sounds: ['/sounds/r/ramp.ogg'] },
+        { foreign: ['clever'], native: ['умный'], sounds: ['/sounds/c/clever.ogg'] },
+        { foreign: ['stupid'], native: ['глупый, тупой'], sounds: ['/sounds/s/stupid.ogg'] },
+        { foreign: ['rude'], native: ['грубый, оскорбительный', 'грубый, резкий'], sounds: ['/sounds/r/rude.ogg'] },
+        { foreign: ['helpful'], native: ['полезный'], sounds: ['/sounds/h/helpful.ogg'] },
+        { foreign: ['feel (felt, felt)'], native: ['осязать', '(разг.) "прощупывать", выяснять положение дел', 'ощущение', 'чувствовать, ощущать'], sounds: ['/sounds/f/feel.ogg'] },
+
+        { foreign: ['theatre'], native: ['театр'], sounds: ['/sounds/t/theatre.ogg'] },
+        { foreign: ['talkative'], native: ['разговорчивый'], sounds: ['/sounds/t/talkative.ogg'] },
+        { foreign: ['jealous'], native: ['ревнивый', 'завидующий, завистливый'], sounds: ['/sounds/j/jealous.ogg'] },
+        { foreign: ['proud'], native: ['гордый', 'испытывающий чувство гордости'], sounds: ['/sounds/p/proud.ogg'] },
+        { foreign: ['confident'], native: ['уверенный в (чём-л.)'], sounds: ['/sounds/c/confident.ogg'] },
+        { foreign: ['hear (heard, heard)'], native: ['слышать, услышать'], sounds: ['/sounds/h/hear.ogg'] },
+
+        { foreign: ['entrance'], native: ['вход, въезд', 'входная дверь'], sounds: ['/sounds/e/entrance.ogg'] },
+        { foreign: ['punctual'], native: ['пунктуальный, точный'], sounds: ['/sounds/p/punctual.ogg'] },
+        { foreign: ['arrogant'], native: ['высокомерный', 'надменный'], sounds: ['/sounds/a/arrogant.ogg'] },
+        { foreign: ['selfish'], native: ['эгоистичный, себялюбивый'], sounds: ['/sounds/s/selfish.ogg'] },
+        { foreign: ['modest'], native: ['скромный'], sounds: ['/sounds/m/modest.ogg'] },
+        { foreign: ['smell'], native: ['запах; аромат', 'незначительное количество; "капелька", "чуточка"', 'пахнуть'], sounds: ['/sounds/s/smell.ogg'] },
+
+        { foreign: ['ladder'], native: ['лестница, трап', 'лезать, взбираться по лестнице'], sounds: ['/sounds/l/ladder.ogg'] },
+        { foreign: ['quiet'], native: ['тихий, бесшумный', 'спокойный, тихий'], sounds: ['/sounds/q/quiet.ogg'] },
+        { foreign: ['shy'], native: ['застенчивый, робкий', 'стеснительный'], sounds: ['/sounds/s/shy.ogg'] },
+        { foreign: ['boring'], native: ['бурение; сверление', 'докучливость, надоедливость'], sounds: ['/sounds/b/boring.ogg'] },
+        { foreign: ['see (saw, seen)'], native: ['видеть', 'видеться, встречаться'], sounds: ['/sounds/s/see.ogg'] },
+        { foreign: ['agree'], native: ['соглашаться ', '(разг.) быть полезным, приятным; быть подходящим'], sounds: ['/sounds/a/agree.ogg'] },
+
+        { foreign: ['lantern'], native: ['фонарь, фонарик', 'освещать, светить фонарём', 'вешать на фонарном столбе'], sounds: ['/sounds/l/lantern.ogg'] },
+        { foreign: ['cheerful'], native: ['весёлый, радостный', 'весёлый; яркий'], sounds: ['/sounds/c/cheerful.ogg'] },
+        { foreign: ['generous'], native: ['великодушный', 'щедрый'], sounds: ['/sounds/g/generous.ogg'] },
+        { foreign: ['polite'], native: ['вежливый, любезный'], sounds: ['/sounds/p/polite.ogg'] },
+        { foreign: ['taste'], native: ['вкус', 'пробовать (на вкус); отведать'], sounds: ['/sounds/t/taste.ogg'] },
+        { foreign: ['believe'], native: ['доверять', 'верить', 'думать, полагать'], sounds: ['/sounds/b/believe.ogg'] },
     ],
     title: 'Lesson 1. Who am I?'
 };
---
+++ server/src/services/bd/block_2/Lesson_2.js
@@ -0,0 +1,34 @@
+// @flow
+
+module.exports = {
+    id: 2,
+    words: [
+        { foreign: ['ramp'], native: ['наклонная плоскость, уклон', 'пандус', 'лежачий полицейский', 'становиться на дыбы, на задние лапы (в геральдике)'], sounds: ['/sounds/r/ramp.ogg'] },
+        { foreign: ['forget (forgot, forgotten)'], native: ['забывать о (чём-л.)'], sounds: ['/sounds/f/forget.ogg'] },
+        { foreign: ['understand (understood, understood)'], native: ['понимать, осмысливать', 'осознать, проникнуться (чем-л.)'], sounds: ['/sounds/u/understand.ogg'] },
+        { foreign: ['need'], native: ['необходимость, надобность, нужда', 'нуждаться (в чём-л.); иметь надобность, потребность'], sounds: ['/sounds/n/need.ogg'] },
+
+
+        { foreign: ['theatre'], native: ['театр'], sounds: ['/sounds/t/theatre.ogg'] },
+        { foreign: ['know (knew, known)'], native: ['знать', 'познать, испытать', 'понимать, осознавать'], sounds: ['/sounds/k/know.ogg'] },
+        { foreign: ['hate'], native: ['ненависть, отвращение', '(разг.) сожалеть, не хотеть, чувствовать себя неловко. I hate to ask him'], sounds: ['/sounds/h/hate.ogg'] },
+        { foreign: ['want'], native: ['необходимость, потребность (в чём-л.)', 'желать, хотеть'], sounds: ['/sounds/w/want.ogg'] },
+
+
+        { foreign: ['entrance'], native: ['вход, въезд', 'входная дверь'], sounds: ['/sounds/e/entrance.ogg'] },
+        { foreign: ['promise'], native: ['обещание', 'обещать, давать обещание'], sounds: ['/sounds/p/promise.ogg'] },
+        { foreign: ['like'], native: ['аналогичный, подобный', 'разг. возможный; вероятный ( they are like to meet again)', 'любить, нравиться', 'хотеть, желать', 'предпочитать'], sounds: ['/sounds/l/like.ogg'] },
+
+
+        { foreign: ['ladder'], native: ['лестница, трап', 'лезать, взбираться по лестнице'], sounds: ['/sounds/l/ladder.ogg'] },
+        { foreign: ['remember'], native: ['помнить, хранить в памяти', 'вспоминать, припоминать, воскрешать в памяти'], sounds: ['/sounds/r/remember.ogg'] },
+        { foreign: ['love'], native: ['любовь, приязнь, симпатия', 'любовь, склонность, тяга (к чему-л.)'], sounds: ['/sounds/l/love.ogg'] },
+
+
+        { foreign: ['lantern'], native: ['фонарь, фонарик', 'освещать, светить фонарём', 'вешать на фонарном столбе'], sounds: ['/sounds/l/lantern.ogg'] },
+        { foreign: ['think (thought ,thought)'], native: ['думать, мыслить', 'размышлять, взвешивать, обдумывать'], sounds: ['/sounds/t/think.ogg'] },
+        { foreign: ['prefer'], native: ['предпочитать'], sounds: ['/sounds/p/prefer.ogg'] },
+
+    ],
+    title: 'Lesson 2. Who am I? (p.2)'
+};
