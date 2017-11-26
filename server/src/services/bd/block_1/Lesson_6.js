5009ae7a4e472ab5ad88e95025f7abcd304df9b2
koskh <koskh@yandex.ru>
2017-11-26 14:22:24 +0100

    #master Lesson 1/6

--- server/src/services/bd/block_1/Lesson_6.js
+++ server/src/services/bd/block_1/Lesson_6.js
@@ -20,9 +20,9 @@ module.exports = {
         { foreign: ['hook'], native: ['крюк, крючок', 'кривой нож; серп', '(разг.) руки-"грабли" '], sounds: ['/sounds/h/hook.ogg'] },
         { foreign: ['finite'], native: ['ограниченный, имеющий предел', 'имеющий ограниченный срок существования; смертный'], sounds: ['/sounds/f/finite.ogg'] },
         { foreign: ['fold'], native: ['сгибать, складывать', 'окутывать', '(тех.) фальцевать'], sounds: ['/sounds/f/fold.ogg'] },
-        { foreign: ['hiking'], native: ['туризм'], sounds: ['/sounds/f/hiking.mp3'] },
-        { foreign: ['dish'], native: ['блюдо, кушанье', '(разг.) красотка, лакомый кусочек', 'подавать еду'], sounds: ['/sounds/d/dish.mp3'] },
-        { foreign: ['suit'], native: ['костюм ', 'комплект, набор (одежды и т.п.)', 'прошение, ходатайство'], sounds: ['/sounds/s/suit.mp3'] },
+        { foreign: ['hiking'], native: ['туризм'], sounds: ['/sounds/h/hiking.mp3'] },
+        { foreign: ['dish'], native: ['блюдо, кушанье', '(разг.) красотка, лакомый кусочек', 'подавать еду'], sounds: ['/sounds/d/dish.ogg'] },
+        { foreign: ['suit'], native: ['костюм ', 'комплект, набор (одежды и т.п.)', 'прошение, ходатайство'], sounds: ['/sounds/s/suit.ogg'] },
 
         { foreign: ['plank'], native: ['толстая и широкая гладко оструганная доска', 'обшивная доска, обшивка корабля', 'планка'], sounds: ['/sounds/p/plank.ogg'] },
         { foreign: ['grant'], native: ['дарить, жаловать, предоставлять', 'разрешать; давать согласие (на ч-л.)', 'акт дарения, подарок, дар'], sounds: ['/sounds/g/grant.ogg'] },
@@ -34,7 +34,7 @@ module.exports = {
         { foreign: ['jacket'], native: ['куртка', 'жакет, пиджак, жилет'], sounds: ['/sounds/j/jacket.ogg'] },
         { foreign: ['gain'], native: ['добывать, зарабатывать', 'выигрывать, добиваться', 'прибыль, выгода'], sounds: ['/sounds/g/gain.ogg'] },
         { foreign: ['handsome'], native: ['красивый, статный (о мужчине)', 'статный, солидный, представительный (о женщине)', 'значительный, большой(цена, прибыль, дар)'], sounds: ['/sounds/h/handsome.ogg'] },
-        { foreign: ['bookcase'], native: ['книжный шкаф; книжная полка; этажерка'], sounds: ['/sounds/b/bookcase.ogg'] },
+        { foreign: ['bookcase'], native: ['книжный шкаф; книжная полка; этажерка'], sounds: ['/sounds/b/bookcase.mp3'] },
         { foreign: ['case'], native: ['случай; история; факт', 'доказательства, аргументация', ' коробка, ящик; контейнер', 'чемодан; портфель; дипломат, кейс'], sounds: ['/sounds/c/case.ogg'] },
         { foreign: ['shuffle'], native: ['смешивать, перемешивать (в беспорядке), тасовать (карты)', ' хитрить, обманывать; вилять, изворачиваться', 'трюк, увёртка, уловка; отговорка', 'ёрзанье'], sounds: ['/sounds/s/shuffle.ogg'] },
 
