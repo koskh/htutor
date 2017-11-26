7ff85f175c7e69da1444988d1cf426efb41239df
koskh <koskh@yandex.ru>
2017-11-26 14:19:12 +0100

    #master Lesson 1/6

---
+++ server/src/services/bd/block_1/Lesson_6.js
@@ -0,0 +1,43 @@
+// @flow
+
+module.exports = {
+    id: 6,
+    words: [
+        { foreign: ['wire'], native: ['проволока', 'электрический, телефонный провод', 'телеграф, телеграмма'], sounds: ['/sounds/w/wire.ogg'] },
+        { foreign: ['hold (held, held)'], native: ['держать', 'содержать в себе, вмещать', '(воен.) удерживать, защищать'], sounds: ['/sounds/h/hold.ogg'] },
+        { foreign: ['forget (forgot, forgotten)'], native: ['забывать', 'пренебрегать (кем-л., чем-л.), игнорировать (кого-л., что-л.)'], sounds: ['/sounds/f/forget.ogg'] },
+        { foreign: ['try'], native: ['пытаться, делать попытку', 'попытка'], sounds: ['/sounds/t/try.ogg'] },
+        { foreign: ['stop'], native: ['остановка, задержка', 'остановка общественного транспорта'], sounds: ['/sounds/s/stop.ogg'] },
+        { foreign: ['at'], native: ['у, в, за, на (нахожд. в некот. точке пространства, воздействия, принадлежн, воздейств) ', 'к, до (указыв на место, где заканчив. процесс) '], sounds: ['/sounds/a/at.ogg'] },
+
+        { foreign: ['modem'], native: ['модем'], sounds: [] },
+        { foreign: ['go on'], native: ['продолжать', 'спешить'], sounds: [] },
+        { foreign: ['remember'], native: ['помнить, хранить в памяти', 'вспоминать, припоминать, воскрешать в памяти'], sounds: ['/sounds/r/remember.ogg'] },
+        { foreign: ['regret'], native: ['сожаление, огорчение', 'раскаяние'], sounds: ['/sounds/r/regret.ogg'] },
+        { foreign: ['cease'], native: ['переставать (делать ч-л.), прекращать', 'прекращаться'], sounds: ['/sounds/c/cease.ogg'] },
+        { foreign: ['dread'], native: ['ужас, благоговейный страх, трепет ', 'страшиться, бояться, трепетать от страха'], sounds: ['/sounds/d/dread.ogg'] },
+
+        { foreign: ['hook'], native: ['крюк, крючок', 'кривой нож; серп', '(разг.) руки-"грабли" '], sounds: ['/sounds/h/hook.ogg'] },
+        { foreign: ['finite'], native: ['ограниченный, имеющий предел', 'имеющий ограниченный срок существования; смертный'], sounds: ['/sounds/f/finite.ogg'] },
+        { foreign: ['fold'], native: ['сгибать, складывать', 'окутывать', '(тех.) фальцевать'], sounds: ['/sounds/f/fold.ogg'] },
+        { foreign: ['hiking'], native: ['туризм'], sounds: ['/sounds/f/hiking.mp3'] },
+        { foreign: ['dish'], native: ['блюдо, кушанье', '(разг.) красотка, лакомый кусочек', 'подавать еду'], sounds: ['/sounds/d/dish.mp3'] },
+        { foreign: ['suit'], native: ['костюм ', 'комплект, набор (одежды и т.п.)', 'прошение, ходатайство'], sounds: ['/sounds/s/suit.mp3'] },
+
+        { foreign: ['plank'], native: ['толстая и широкая гладко оструганная доска', 'обшивная доска, обшивка корабля', 'планка'], sounds: ['/sounds/p/plank.ogg'] },
+        { foreign: ['grant'], native: ['дарить, жаловать, предоставлять', 'разрешать; давать согласие (на ч-л.)', 'акт дарения, подарок, дар'], sounds: ['/sounds/g/grant.ogg'] },
+        { foreign: ['juice'], native: ['сок', 'смысл, сущность, соль (чего-л.)', 'выжимать, давить сок', '(разг.) квасить, много пить'], sounds: ['/sounds/j/juice.ogg'] },
+        { foreign: ['useful'], native: ['полезный, пригодный', '(разг.) способный; успешный; весьма похвальный'], sounds: ['/sounds/u/useful.ogg'] },
+        { foreign: ['ugly'], native: ['безобразный, уродливый', 'мерзкий, ужасный, неприятный; противный'], sounds: ['/sounds/u/ugly.ogg'] },
+        { foreign: ['foreign'], native: ['иностранный; заграничный', 'чужой, незнакомый'], sounds: ['/sounds/f/foreign.ogg'] },
+
+        { foreign: ['jacket'], native: ['куртка', 'жакет, пиджак, жилет'], sounds: ['/sounds/j/jacket.ogg'] },
+        { foreign: ['gain'], native: ['добывать, зарабатывать', 'выигрывать, добиваться', 'прибыль, выгода'], sounds: ['/sounds/g/gain.ogg'] },
+        { foreign: ['handsome'], native: ['красивый, статный (о мужчине)', 'статный, солидный, представительный (о женщине)', 'значительный, большой(цена, прибыль, дар)'], sounds: ['/sounds/h/handsome.ogg'] },
+        { foreign: ['bookcase'], native: ['книжный шкаф; книжная полка; этажерка'], sounds: ['/sounds/b/bookcase.ogg'] },
+        { foreign: ['case'], native: ['случай; история; факт', 'доказательства, аргументация', ' коробка, ящик; контейнер', 'чемодан; портфель; дипломат, кейс'], sounds: ['/sounds/c/case.ogg'] },
+        { foreign: ['shuffle'], native: ['смешивать, перемешивать (в беспорядке), тасовать (карты)', ' хитрить, обманывать; вилять, изворачиваться', 'трюк, увёртка, уловка; отговорка', 'ёрзанье'], sounds: ['/sounds/s/shuffle.ogg'] },
+
+    ],
+    title: 'Lesson 6. Modem and jacket.'
+};
--- server/src/services/bd/block_1/index.js
+++ server/src/services/bd/block_1/index.js
@@ -5,6 +5,7 @@ const Lesson2 = require('./Lesson_2');
 const Lesson3 = require('./Lesson_3');
 const Lesson4 = require('./Lesson_4');
 const Lesson5 = require('./Lesson_5');
+const Lesson6 = require('./Lesson_6');
 
 // eslint-disable-next-line
 const lessons: Array<Lesson> = [
@@ -13,10 +14,11 @@ const lessons: Array<Lesson> = [
     Lesson3,
     Lesson4,
     Lesson5,
+    Lesson6
 ];
 
 module.exports = {
     id: 1,
-    title: 'Техн запомин "Джордано"',
+    title: 'Базовые слова',
     lessons
 };
