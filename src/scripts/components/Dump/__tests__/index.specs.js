93c3835a4b0d46ef2bd31a626bb6b11a5e256df0
koskh <koskh@yandex.ru>
2017-12-25 17:25:41 +0100

    #master from Mocha to Jest, jest-image-snapshot

--- src/scripts/components/Dump/__tests__/index.specs.js
+++ src/scripts/components/Dump/__tests__/index.specs.js
@@ -3,10 +3,10 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import {toMatchImageSnapshot} from 'jest-image-snapshot';
-expect.extend({ toMatchImageSnapshot });
+// import {toMatchImageSnapshot} from 'jest-image-snapshot';
+// expect.extend({ toMatchImageSnapshot });
 
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 
 import Dump from '../index';
 
@@ -46,26 +46,26 @@ test('components/Dump renders correctly snapshot', ()=>{
 
 });
 
-test('renders correctly', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('http://192.168.1.8:3001');
-    await  page.waitForSelector('.qa__home--body');
-    const screenshot = await page.screenshot();
-    expect(screenshot).toMatchImageSnapshot();
-
-    await page.goto('http://192.168.1.8:3001/home');
-    await  page.waitForSelector('.qa__home--body');
-    const screenshot1 = await page.screenshot();
-    expect(screenshot1).toMatchImageSnapshot();
-
-    await page.goto('http://192.168.1.8:3001/learn/1');
-    await  page.waitForSelector('.qa__learn--body');
-    const screenshot2 = await page.screenshot();
-    expect(screenshot2).toMatchImageSnapshot();
-
-    // expect(screenshot).toMatchImageSnapshot();
-
-    await browser.close();
-});
+// test('renders correctly', async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//
+//     await page.goto('http://192.168.1.8:3001');
+//     await  page.waitForSelector('.qa__home--body');
+//     const screenshot = await page.screenshot();
+//     expect(screenshot).toMatchImageSnapshot();
+//
+//     await page.goto('http://192.168.1.8:3001/home');
+//     await  page.waitForSelector('.qa__home--body');
+//     const screenshot1 = await page.screenshot();
+//     expect(screenshot1).toMatchImageSnapshot();
+//
+//     await page.goto('http://192.168.1.8:3001/learn/1');
+//     await  page.waitForSelector('.qa__learn--body');
+//     const screenshot2 = await page.screenshot();
+//     expect(screenshot2).toMatchImageSnapshot();
+//
+//     // expect(screenshot).toMatchImageSnapshot();
+//
+//     await browser.close();
+// });
